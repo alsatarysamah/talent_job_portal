deb37ba7291fbd44f1f24086f1a74582cf8990e5
alsatarysamah <alsatary.samah@yahoo.com>
2024-08-30 23:40:06 +0300

    add style to the  main continer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 
-
+import styles from "../styles/Home.module.css";
+import Navbar from "../src/component/Navbar";
+const {mainContainer}=styles
 export default function Home() {
   return (
-    <div >
-   
-    
+    <div className={mainContainer}>
+
+
+
     </div>
-  )
+  );
 }
