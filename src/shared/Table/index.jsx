95331dbda0e207540b08df83d2e92b6c01ce1746
alsatarysamah <alsatary.samah@yahoo.com>
2024-08-31 14:17:15 +0300

    install react app

---
+++ src/shared/Table/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useTable } from "react-table";
+import styles from "./style.module.css";
+
+
+
+const Table = ({columns,data}) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+  return (
+    <table {...getTableProps()} className={styles.table}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()} key={column.id}>
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()} className={styles.row} key={row.id}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()} key={cell.column.id}>
+                  {cell.render("Cell")}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
