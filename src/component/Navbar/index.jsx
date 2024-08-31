adce3b3f495b0336f761502b873e3d89f60727bd
alsatarysamah <alsatary.samah@yahoo.com>
2024-08-31 11:41:24 +0300

    implement navbar

---
+++ src/component/Navbar/index.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import styles from "./style.module.css";
+import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import BadgeOutlinedIcon from "@mui/icons-material/BadgeOutlined";
+import { useRouter } from "next/router";
+const { container, leftSide, logo, job } = styles;
+function Navbar() {
+  const router = useRouter();
+  const handleJobClick = () => {
+    router.push("/job");
+  };
+  return (
+    <div className={container}>
+      <div className={leftSide}>
+        <div className={logo}>
+          <Image src="/images/logo.png" height={44} width={44} alt="Logo" />
+        </div>
+        <div className={job} onClick={handleJobClick}>
+          <LocalMallOutlinedIcon />
+         <div>Jobs</div> 
+        </div>
+        <div className={job}>
+          <BadgeOutlinedIcon />
+         
+         <div> Talent pool</div> 
+
+        </div>
+      </div>
+      <div>Element 4</div>
+    </div>
+  );
+}
+
+export default Navbar;
